e87b84ecfdecc8d0ad77003e420dc61d70942014
Kasey Jones <krjones9@ncsu.edu>
2017-04-03 18:33:18 -0400

    Adding CSV files and new application

--- shiny/ui.R
+++ shiny/ui.R
@@ -14,8 +14,8 @@ sidebarLayout(
   sidebarPanel(width = 3,
 
                selectInput("network", "Select Network:",
-                           choices = c("Top 100 Action Movies from 2000-2017" = "option1",
-                                       "Top 100 Comedy Movies from 2000-2017" = "option2",
+                           choices = c("Top 70 Action Movies from 2000-2017" = "option1",
+                                       "Top 70 Comedy Movies from 2000-2017" = "option2",
                                        "Custom Network (use selections below)" = "custom"),
                            selected = "option1"),
 
@@ -27,28 +27,33 @@ sidebarLayout(
 
                conditionalPanel(
                  condition = "input.network == 'custom'",
-
-               uiOutput("genres"),
+                 
+                 uiOutput("genres"),
 
                fluidRow(
-                 column(6,numericInput("moviecount", "Number of Movies:",
+                 column(6,numericInput("movie_count", "Number of Movies:",
                               20, min = 20, max = 150)),
-                 column(6,numericInput("actorcount", "Number of Actors:",
-                              7, min = 5, max = 15))
+                 column(6,numericInput("actor_count", "Number of People:",
+                              7, min = 1, max = 20))
                  ),
 
                dateRangeInput('dateRange',
                                 label = 'Date range for Movies:',
                                 start = "2010/01/01", end = "2015/12/31"
-                 )),
+                 ),
+               
+               actionButton("goButton", "Find Movies:")
+               ),
 
                br(),
 
-               actionButton("goButton", "Find Movies:")
+               helpText("----------"),
+               helpText("----------"),
+               uiOutput("actors")
                 ),
 
             mainPanel(
-                #tableOutput("table")
+            
               tabsetPanel(
                 tabPanel("Network Diagram",
                   forceNetworkOutput("force", height = "660px", width = "110%")),
